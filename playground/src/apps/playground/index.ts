7cf175201dbdaeb6930293545d96ff6cf46babd5
Ribbit <echo07200503@gmail.com>
2023-10-13 19:21:42 +0900

    🎉 テスト環境を拡充

--- playground/src/apps/playground/index.ts
+++ playground/src/apps/playground/index.ts
@@ -1,27 +1,43 @@
-import { LoadingOverlay } from "@konomi-app/ui";
+import { Modal } from "../../../../vanilla/dist";
 import "./react";
 
 console.log("javascript file loaded!");
 
 document.addEventListener("DOMContentLoaded", () => {
-  const loadingOverlay = new LoadingOverlay();
+  const modal = new Modal();
 
   const loading = () => {
-    loadingOverlay.label = "Loading...";
-    loadingOverlay.show();
+    modal.label = "Loading...";
+    modal.loading();
 
     for (let i = 0; i < 5; i++) {
       setTimeout(() => {
-        loadingOverlay.label = `ステップ${i + 1}を実行中...`;
-        loadingOverlay.progress = (i + 1) * 20;
+        modal.label = `ステップ${i + 1}を実行中...`;
+        modal.progress = (i + 1) * 20;
       }, i * 1200);
     }
     setTimeout(() => {
-      loadingOverlay.html = `<div><ul><li>1</li><li>2</li><li>3</li></ul></div>`;
+      modal.html = `<div><ul><li>1</li><li>2</li><li>3</li></ul></div>`;
     }, 5000);
+  };
+
+  const showModal = () => {
+    modal.alert({
+      title: "タイトル",
+      text: "テキスト",
+      icon: "success",
+    });
+    modal.show();
+
+    for (let i = 0; i < 5; i++) {
+      setTimeout(() => {
+        modal.label = `ステップ${i + 1}を実行中...`;
+        modal.progress = (i + 1) * 20;
+      }, i * 1200);
+    }
     setTimeout(() => {
-      loadingOverlay.hide();
-    }, 6200);
+      modal.html = `<div><ul><li>1</li><li>2</li><li>3</li></ul></div>`;
+    }, 5000);
   };
 
   const button = document.createElement("button");
@@ -30,5 +46,25 @@ document.addEventListener("DOMContentLoaded", () => {
   button.addEventListener("click", () => {
     console.log("clicked");
     loading();
+  });
+
+  const button2 = document.createElement("button");
+  button2.textContent = "modal";
+  document.body.append(button2);
+  button2.addEventListener("click", () => {
+    console.log("clicked");
+    showModal();
+  });
+
+  const fab = document.createElement("button");
+  fab.style.position = "fixed";
+  fab.style.bottom = "20px";
+  fab.style.right = "20px";
+  fab.style.padding = "8px 16px";
+  fab.style.zIndex = "10000";
+  fab.textContent = "hide";
+  document.body.append(fab);
+  fab.addEventListener("click", () => {
+    modal.hide();
   });
 });
